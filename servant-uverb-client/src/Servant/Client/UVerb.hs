cb09ca773a92881048dfbbcc02b97384df1a67b7
Arian van Putten <arian.vanputten@gmail.com>
2019-09-07 12:31:41 +0200

    Do correct error messaging on parse failure

--- servant-uverb-client/src/Servant/Client/UVerb.hs
+++ servant-uverb-client/src/Servant/Client/UVerb.hs
@@ -5,34 +5,51 @@ module Servant.Client.UVerb () where
 import Data.ByteString.Lazy (ByteString)
 import Data.Foldable (toList)
 import Data.Proxy (Proxy(Proxy))
-import Data.SOP.BasicFunctors ((:.:)(Comp))
+import Data.SOP.BasicFunctors (I(I),(:.:)(Comp))
 import Data.SOP.Constraint(All, And, Compose)
 
+import GHC.TypeLits
 import Data.SOP.NP (NP(..), cpure_NP)
 import Data.SOP.NS (NS(..))
 import Servant.API.ContentTypes (MimeUnrender(mimeUnrender), Accept, contentTypes)
 import Servant.API (ReflectMethod(reflectMethod))
-import Servant.API.UVerb (UVerb, MakesUVerb, HasStatus, MakesResource, inject)
-import Servant.Client.Core (HasClient(Client, hoistClientMonad, clientWithRoute), RunClient, runRequest, requestMethod, responseStatusCode, responseBody, requestAccept)
+import Servant.API.UVerb (UVerb, MakesUVerb, HasStatus(..), MakesResource(..), inject)
+import Servant.Client.Core (ServantError(..),HasClient(Client, hoistClientMonad, clientWithRoute), RunClient(..), runRequest, requestMethod, responseStatusCode, responseBody, requestAccept)
+
+import Servant.Client.Core.Internal.RunClient (checkContentTypeHeader)
 
 import qualified Data.Sequence as Seq
+import qualified Data.Text as T
+import           Network.HTTP.Media                   (matches)
+import Control.Monad (unless)
 
--- TODO this is some kind of WriterT I suppose?.
--- Ask Andres to Code Golf the shit out of this
+proxyOf :: a -> Proxy a
+proxyOf _ = Proxy
+
+proxyOf' :: f a -> Proxy a
+proxyOf' _ = Proxy
+
 -- | Given a list of parsers of 'mkres', returns the first one that succeeds and all the
 -- failures it encountered along the way
-tryParsers' :: err -> NP (Either err :.: mkres) xs -> ([err], Maybe (NS mkres xs))
+tryParsers' :: All (HasStatus mkres) xs => err -> NP (Either err :.: mkres) xs -> ([err], Maybe (NS mkres xs))
 tryParsers' err Nil =  ([err], Nothing)
-tryParsers' _ (x :* Nil) =
+tryParsers' _ (Comp x :* Nil) =
   case x of
-    Comp (Left err) -> ([err], Nothing)
-    Comp (Right res) -> ([], Just (Z res))
-tryParsers' _ (x :* xs) =
+    Left err -> ([err], Nothing)
+    Right res ->  ([], Just (Z res))
+tryParsers' _ (Comp x :* xs) =
+  -- Let us check what the associated http status is for this resource.  If it
+  -- doesn't match the one that the server responded with, we know we can skip
+  -- this parse
+  let status' = getStatus (proxyOf' x)
+  in
+  -- force the computation of the parser only if the status matches (optimization)
   case x of
-    Comp (Left err') -> 
+    Left err' -> 
       let (err'', res) = tryParsers' err' xs
       in (err' : err'', S <$> res)
-    Comp (Right res) -> ([], Just $ inject res)
+    Right res -> ([], Just $ inject res)
+
 
 type IsResource ct mkres =
     (MimeUnrender ct `Compose` mkres) `And`
@@ -62,11 +79,15 @@ instance
     let accept = Seq.fromList . toList . contentTypes $ Proxy @ct
     let method = reflectMethod $ Proxy @method
     response <- runRequest request { requestMethod = method, requestAccept = accept }
-    let _status = responseStatusCode response
+    responseContentType <- checkContentTypeHeader response
+    unless (any (matches responseContentType) accept) $
+      throwServantError $ UnsupportedContentType responseContentType response
+
+    let status = responseStatusCode response
     let body = responseBody response
-    let (err, res) = tryParsers' "???" . mimeUnrenders @mkres @ct @resources $ body
+    let (errors, res) = tryParsers' "no content types (shouldn't happen?)" . mimeUnrenders @mkres @ct @resources $ body
     case res of
-      Nothing -> error (show err)
+      Nothing -> throwServantError $ DecodeFailure (T.pack (show errors)) response
       Just x -> return x
   hoistClientMonad Proxy Proxy nt s = nt s
 
