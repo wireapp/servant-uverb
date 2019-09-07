a2b35a4cf2c4908efccc69978ee1426fc21dd95c
Arian van Putten <arian.vanputten@gmail.com>
2019-09-07 16:39:23 +0200

    Add servant-uverb-mock (#6)
    * Add servant-uverb-mock

---
+++ servant-uverb-mock/src/Servant/Mock/UVerb.hs
@@ -0,0 +1,28 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Servant.Mock.UVerb () where
+
+import Control.Monad.IO.Class (liftIO)
+import Data.Proxy (Proxy(Proxy))
+import Data.SOP.BasicFunctors ((:.:)(Comp))
+import Data.SOP.Constraint(All, Compose)
+import Data.SOP.NP (cpure_NP) 
+import Data.SOP.NS (NS(..), apInjs_NP, sequence'_NS)
+import Servant.API.UVerb
+import Servant.Mock
+import Servant.Server (HasServer)
+import Test.QuickCheck (Gen, Arbitrary(arbitrary), generate, elements)
+
+
+arbitraryNS :: forall f xs. All (Arbitrary `Compose` f) xs =>  Gen (NS f xs)
+arbitraryNS =
+  sequence'_NS =<< elements (apInjs_NP (cpure_NP (Proxy @(Arbitrary `Compose` f)) (Comp arbitrary)))
+
+
+instance 
+  ( All (Compose Arbitrary mkres) resources
+  , HasServer (UVerb mkres method cts resources) ctx 
+  ) => HasMock (UVerb mkres method cts resources) ctx where
+  mock _ _ = liftIO (generate arbitraryNS)
+
+
+
