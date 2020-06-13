3f6e327d6b69781f9541281155fa25918f038dc9
Matthias Fischmann <mf@zerobuzz.net>
2020-06-13 16:51:37 +0200

    ...

--- handler.hs
+++ handler.hs
@@ -27,50 +27,76 @@
 #-}
 
 import Network.HTTP.Types(Status, status203)
-import Servant.API (StdMethod(..), JSON)
+import Servant.API (StdMethod(..), JSON, (:<|>), (:>))
 import Servant.Server
 import GHC.TypeLits
 import Data.SOP.NS
+import Control.Monad.Identity
 import Data.Proxy
+import Servant.API.UVerb.OpenUnion
+
 
 -- * Servant.API.Status
+--
+
 class KnownNat n => KnownStatus n where
   statusVal :: proxy n -> Status
-
-
-class  KnownStatus (TheStatus a) => HasStatus (a :: *) where
-  type TheStatus (a :: *) :: Nat
 
 instance KnownStatus 203 where
   statusVal = const status203
 
 
+-- * Servant.API.UVerb
+--
+
+class  KnownStatus (StatusOf a) => HasStatus (a :: *) where
+  type StatusOf (a :: *) :: Nat
+
 status :: forall a proxy. HasStatus a => proxy a -> Status
-status = const (statusVal (Proxy :: Proxy (TheStatus a)))
+status = const (statusVal (Proxy :: Proxy (StatusOf a)))
 
 newtype WithStatus (k :: Nat) a = WithStatus a
 
 instance KnownStatus n => HasStatus (WithStatus n a) where
-  type TheStatus (WithStatus n a) = n
+  type StatusOf (WithStatus n a) = n
 
---
+-- FUTUREWORK:
+-- @type Verb method statusCode contentTypes a = UVerb method contentTypes [WithStatus statusCode a]@
+data UVerb (method :: StdMethod) (contentTypes :: [*]) (as :: [*])
 
 
 -- * Servant.API.UVerb
-data UVerb (method :: StdMethod) (cts :: [*]) (resources :: [*])
+--
 
------------
+-- | 'return' for 'UVerb' handlers.  Takes a value of any of the members of the open union,
+-- and will construct a union value in an 'Applicative' (eg. 'Server').
+respond
+  :: forall (f :: * -> *) (x :: *) (xs :: [*]). (Applicative f, HasStatus x, IsMember x xs)
+  => x -> f (NS Identity xs)
+respond = pure . inject . Identity
+
+
+
+
+-- * example use case
+--
 
 data FisxUser = FisxUser { name :: String }
 
+-- | we can get around 'WithStatus' if we want to, and associate the status code with our
+-- resource types directly.
+--
+-- (to avoid orphan instances and make it more explicit what's in the API and what isn't, we
+-- could introduce a newtype 'Resource' that wraps all the types we're using in our routing
+-- table, and then define lots of 'HasStatus' instances for @Resource This@ and @Resource
+-- That@.)
 instance HasStatus FisxUser where
-  type TheStatus FisxUser = 203
-
-type FisxAPI = UVerb 'GET '[JSON] '[FisxUser]
+  type StatusOf FisxUser = 203
 
 data ArianUser = ArianUser
 
-type ArianAPI = UVerb 'GET '[JSON] '[WithStatus 201 ArianUser]
+type API = "fisx" :> UVerb 'GET '[JSON] '[FisxUser, WithStatus 303 String]
+      :<|> "arian" :> UVerb 'GET '[JSON] '[WithStatus 201 ArianUser]
 
 
 
@@ -88,9 +114,8 @@ instance  HasServer (Get ctypes xs) where
 -}
 
 
+main :: IO ()
 main = undefined
 
--- handler' :: Server API'  -- ~  IO (NS I '[WithStatus 500 Bool, WithStatus 403 Bool])
--- handler' = pure $ injectNS $ WithStatus' "error"
-
-
+handler :: Server API
+handler = undefined
