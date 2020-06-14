f2d296285772b8bc15fad15708774d97be287975
Nathan van Doorn <nvd1234@gmail.com>
2020-06-14 15:49:16 +0100

    Fix swagger combination

--- servant-uverb-swagger/src/Servant/Swagger/UVerb.hs
+++ servant-uverb-swagger/src/Servant/Swagger/UVerb.hs
@@ -2,11 +2,45 @@
 {-# OPTIONS_GHC -Wno-orphans #-}
 module Servant.Swagger.UVerb () where
 
+import Control.Applicative ((<|>))
+import qualified Data.HashMap.Strict.InsOrd as I
 import Data.Proxy (Proxy (Proxy))
-import Data.Swagger (ToSchema)
+import Data.Swagger (ToSchema, Swagger(..), PathItem(..))
 import Servant.API.Verbs (Verb)
 import Servant.API.UVerb (HasStatus, StatusOf, UVerb, WithStatus)
 import Servant.Swagger.Internal (AllAccept, HasSwagger(..), SwaggerMethod)
+
+-- workaround for https://github.com/GetShopTV/swagger2/issues/218
+-- We'd like to juse use (<>) but the instances are wrong
+combinePathItem :: PathItem -> PathItem -> PathItem
+combinePathItem s t = PathItem
+  { _pathItemGet = _pathItemGet s <> _pathItemGet t
+  , _pathItemPut = _pathItemPut s <> _pathItemPut t
+  , _pathItemPost = _pathItemPost s <> _pathItemPost t
+  , _pathItemDelete = _pathItemDelete s <> _pathItemDelete t
+  , _pathItemOptions = _pathItemOptions s <> _pathItemOptions t
+  , _pathItemHead = _pathItemHead s <> _pathItemHead t
+  , _pathItemPatch = _pathItemPatch s <> _pathItemPatch t
+  , _pathItemParameters = _pathItemParameters s <> _pathItemParameters t
+  }
+
+combineSwagger :: Swagger -> Swagger -> Swagger
+combineSwagger s t = Swagger
+  { _swaggerInfo = _swaggerInfo s <> _swaggerInfo t
+  , _swaggerHost = _swaggerHost s <|> _swaggerHost t
+  , _swaggerBasePath = _swaggerBasePath s <> _swaggerBasePath t
+  , _swaggerSchemes = _swaggerSchemes s <> _swaggerSchemes t
+  , _swaggerConsumes = _swaggerConsumes s <> _swaggerConsumes t
+  , _swaggerProduces = _swaggerProduces s <> _swaggerProduces t
+  , _swaggerPaths = I.unionWith combinePathItem (_swaggerPaths s) (_swaggerPaths t)
+  , _swaggerDefinitions = _swaggerDefinitions s <> _swaggerDefinitions t
+  , _swaggerParameters = _swaggerParameters s <> _swaggerParameters t
+  , _swaggerResponses = _swaggerResponses s <> _swaggerResponses t
+  , _swaggerSecurityDefinitions = _swaggerSecurityDefinitions s <> _swaggerSecurityDefinitions t
+  , _swaggerSecurity = _swaggerSecurity s <> _swaggerSecurity t
+  , _swaggerTags = _swaggerTags s <> _swaggerTags t
+  , _swaggerExternalDocs = _swaggerExternalDocs s <> _swaggerExternalDocs t
+  }
 
 instance HasSwagger (UVerb method cs '[]) where
   toSwagger _ = mempty
@@ -22,6 +56,6 @@ instance
   where
   toSwagger _ =
     toSwagger (Proxy :: Proxy (Verb method (StatusOf a) cs a))
-      <> toSwagger (Proxy :: Proxy (UVerb method cs as))
+      `combineSwagger` toSwagger (Proxy :: Proxy (UVerb method cs as))
 
 instance ToSchema a => ToSchema (WithStatus s a) where
