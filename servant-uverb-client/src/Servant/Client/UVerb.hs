c84e9a0d65f4e9828cea1d8be6453dba16e0a0b0
Matthias Fischmann <mf@zerobuzz.net>
2020-06-14 14:02:31 +0200

    client.

--- servant-uverb-client/src/Servant/Client/UVerb.hs
+++ servant-uverb-client/src/Servant/Client/UVerb.hs
@@ -1,112 +1,115 @@
-{-# OPTIONS_GHC -Wno-orphans -Wno-unused-imports #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
-module Servant.Client.UVerb () where
+{-# OPTIONS_GHC -Wno-orphans #-}
 
-import Data.ByteString.Lazy (ByteString)
+module Servant.Client.UVerb
+  ( collapseUResp,
+    extractUResp,
+    ClientParseError (..),
+  )
+where
+
+import Control.Arrow ((+++), left)
+import Control.Monad (unless)
+import qualified Data.ByteString.Lazy as LB
+import Data.Either (partitionEithers)
 import Data.Foldable (toList)
-import Data.Proxy (Proxy(Proxy))
-import Data.SOP.BasicFunctors ((:.:)(Comp))
-import Data.SOP.Constraint(All, And, Compose)
-
-import Data.SOP.NP (NP(..), cpure_NP)
-import Data.SOP.NS (NS(..))
-import Servant.API.ContentTypes (MimeUnrender(mimeUnrender), Accept, contentTypes)
-import Servant.API (ReflectMethod(reflectMethod))
-import Servant.API.UVerb (UVerb, MakesUVerb, HasStatus(..), MakesResource(..), inject)
-import Servant.Client.Core (ClientError(..), HasClient(Client, hoistClientMonad, clientWithRoute), RunClient(..), runRequest, requestMethod, responseStatusCode, responseBody, requestAccept)
-
-import Servant.Client.Core.Response
-import Network.HTTP.Media (MediaType, parseAccept, (//))
-
+import Data.Functor.Identity (Identity (Identity), runIdentity)
+import Data.SOP.BasicFunctors ((:.:) (Comp), K (K))
+import Data.SOP.Constraint (All, Constraint)
+import Data.SOP.NP (NP (..), cpure_NP)
+import Data.SOP.NS (NS (S), cmap_NS, collapse_NS)
 import qualified Data.Sequence as Seq
 import qualified Data.Text as T
-import           Network.HTTP.Media                   (matches)
-import           Network.HTTP.Types (Status)
-import Control.Monad (unless)
+import Data.Typeable (Proxy (Proxy), Typeable, cast)
+import qualified GHC.Generics as GHC
+import Network.HTTP.Media ((//), MediaType, matches, parseAccept)
+import Network.HTTP.Types (Status)
+import Servant (ReflectMethod, reflectMethod)
+import Servant.API.ContentTypes (AllMime (allMime), AllMimeUnrender (allMimeUnrender))
+import Servant.API.UVerb (HasStatus, Statuses, UVerb, Union, Unique, inject, statusOf)
+import Servant.Client (ClientError (..), HasClient (..), Response, responseBody, responseHeaders, responseStatusCode)
+import Servant.Client.Core (RunClient (..), requestAccept, requestMethod, runRequest)
 
+-- | convenience function to extract an unknown union element using a type class.
+collapseUResp ::
+  forall c a as.
+  All c as =>
+  Proxy (c :: * -> Constraint) ->
+  (forall x. c x => x -> a) ->
+  Union as ->
+  a
+collapseUResp proxy render = collapse_NS . cmap_NS proxy (K . render . runIdentity)
+
+-- | convenience function to extract an unknown union element using 'cast'.
+extractUResp :: forall a as. (All Typeable as, Typeable a) => Union as -> Maybe a
+extractUResp = collapse_NS . cmap_NS (Proxy @Typeable) (K . cast . runIdentity)
+
+data ClientParseError = ClientParseError MediaType String | ClientStatusMismatch | ClientNoMatchingStatus
+  deriving (Eq, Show, GHC.Generic)
+
+instance
+  ( RunClient m,
+    contentTypes ~ (contentType ': contentTypes'), -- TODO: can we to _ instead of contentTypes'?  probably not.
+    as ~ (a ': as'),
+    AllMime contentTypes,
+    ReflectMethod method,
+    All (AllMimeUnrender contentTypes) as,
+    All HasStatus as,
+    Unique (Statuses as)
+  ) =>
+  HasClient m (UVerb method contentTypes as)
+  where
+  type Client m (UVerb method contentTypes as) = m (Union as)
+
+  clientWithRoute _ _ request = do
+    let accept = Seq.fromList . allMime $ Proxy @contentTypes
+        -- TODO(fisx): we want to send an accept header with, say, the first content type
+        -- supported by the api, so we don't have to parse all of them, no?  not sure i'm
+        -- missing anything here.
+
+        method = reflectMethod $ Proxy @method
+    response <- runRequest request {requestMethod = method, requestAccept = accept}
+    responseContentType <- checkContentTypeHeader response
+    unless (any (matches responseContentType) accept)
+      $ throwClientError
+      $ UnsupportedContentType responseContentType response
+
+    let status = responseStatusCode response
+        body = responseBody response
+        res = tryParsers status $ mimeUnrenders (Proxy @contentTypes) body
+    case res of
+      Left errors -> throwClientError $ DecodeFailure (T.pack (show errors)) response
+      Right x -> return x
+
+  hoistClientMonad _ _ nt s = nt s
 
 -- | Copied from "Servant.Client.Core.HasClient".
 checkContentTypeHeader :: RunClient m => Response -> m MediaType
 checkContentTypeHeader response =
   case lookup "Content-Type" $ toList $ responseHeaders response of
-    Nothing -> return $ "application"//"octet-stream"
+    Nothing -> return $ "application" // "octet-stream"
     Just t -> case parseAccept t of
       Nothing -> throwClientError $ InvalidContentTypeHeader response
       Just t' -> return t'
 
-
-proxyOf' :: f a -> Proxy a
-proxyOf' _ = Proxy
-
 -- | Given a list of parsers of 'mkres', returns the first one that succeeds and all the
 -- failures it encountered along the way
--- TODO: the Nil base-case is never reached because Content types cannot be non-empty in servant
-tryParsers' :: All (HasStatus mkres) xs => Status -> NP (Either String :.: mkres) xs -> ([String], Maybe (NS mkres xs))
-tryParsers' _ Nil =  (["This should've never happened"], Nothing)
-tryParsers' _ (Comp x :* Nil) =
-  case x of
-    Left err -> ([err], Nothing)
-    Right res ->  ([], Just (Z res))
-tryParsers' status (Comp x :* xs) =
-  -- I am open to suggestions to format this code in a less alien-like way
-  let
-    status' = getStatus (proxyOf' x)
-  in
-    if status == status'
-    then
-      case x of
-        Left err' ->
-          let
-            (err'', res) = tryParsers' status xs
-          in
-            (err' : err'', S <$> res)
-        Right res -> ([], Just $ inject res)
-    else -- no reason to parse in the first place. This ain't the one we're looking for
-        let
-          (err, res) = tryParsers' status xs
-        in
-          ("Status did not match" : err, S <$> res)
-
-
-type IsResource ct mkres =
-    (MimeUnrender ct `Compose` mkres) `And`
-    HasStatus mkres `And`
-    MakesResource mkres
+-- TODO; better name, rewrite haddocs.
+tryParsers :: All HasStatus as => Status -> NP ([] :.: Either (MediaType, String)) as -> Either [ClientParseError] (Union as)
+tryParsers _ Nil = Left [ClientNoMatchingStatus]
+tryParsers status (Comp x :* xs)
+  | status == statusOf (Comp x) =
+    case partitionEithers x of
+      (err', []) -> (map (uncurry ClientParseError) err' ++) +++ S $ tryParsers status xs
+      (_, (res : _)) -> Right . inject . Identity $ res
+  | otherwise = -- no reason to parse in the first place. This ain't the one we're looking for
+    (ClientStatusMismatch :) +++ S $ tryParsers status xs
 
 -- | Given a list of types, parses the given response body as each type
-mimeUnrenders
-  :: forall mkres ct xs . All (IsResource ct mkres) xs
-  => ByteString -> NP (Either String :.: mkres) xs
-mimeUnrenders body = cpure_NP (Proxy @(IsResource ct mkres)) (Comp $ mimeUnrender (Proxy @ct) body)
-
--- We are the client, so we're free to pick whatever content type we like!
--- we'll pick the first one
--- If the list of resources is _empty_ we assume the return type is NoContent, and the status code _must_ be 401
--- TODO: Implement that behaviour on the server
-instance
-  ( RunClient m
-  , cts ~ ( ct ': cts')
-  , resources ~ (res ': resources')  -- make sure the user provides a list of resources
-  , Accept ct
-  , ReflectMethod method
-  , All (IsResource ct mkres) resources
-  , MakesUVerb mkres method cts resources
-  ) => HasClient m (UVerb  mkres method cts resources) where
-
-  type Client m (UVerb mkres method cts resources) = m (NS mkres resources)
-
-  clientWithRoute Proxy Proxy request = do
-    let accept = Seq.fromList . toList . contentTypes $ Proxy @ct
-    let method = reflectMethod $ Proxy @method
-    response <- runRequest request { requestMethod = method, requestAccept = accept }
-    responseContentType <- checkContentTypeHeader response
-    unless (any (matches responseContentType) accept) $
-      throwClientError $ UnsupportedContentType responseContentType response
-
-    let status = responseStatusCode response
-    let body = responseBody response
-    let (errors, res) = tryParsers' status  . mimeUnrenders @mkres @ct @resources $ body
-    case res of
-      Nothing -> throwClientError $ DecodeFailure (T.pack (show errors)) response
-      Just x -> return x
-  hoistClientMonad Proxy Proxy nt s = nt s
+mimeUnrenders ::
+  forall contentTypes as.
+  All (AllMimeUnrender contentTypes) as =>
+  Proxy contentTypes ->
+  LB.ByteString ->
+  NP ([] :.: Either (MediaType, String)) as
+mimeUnrenders ctp body = cpure_NP (Proxy @(AllMimeUnrender contentTypes)) (Comp . map (\(mediaType, parser) -> left ((,) mediaType) (parser body)) . allMimeUnrender $ ctp)
